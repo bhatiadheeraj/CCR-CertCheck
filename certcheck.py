1fd6819ec417af7dd47ad0c7bb7a8bd87041d1c6
Dheeraj <dheerajb@buffalo.edu>
2020-06-11 10:43:01 -0400

    getting attrib

--- certcheck.py
+++ certcheck.py
@@ -38,5 +38,4 @@ run_command(command)
 tree = ET.parse('test.xml')
 root = tree.getroot()
 for name in root.iter("hostname"):
-    a = ElementTree.dump(name)
-    print(a)
+    a = ElementTree.dump(name.attrib)
