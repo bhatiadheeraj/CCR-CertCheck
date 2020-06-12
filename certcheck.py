5298e9322d01bd24f6339dee79613222fb72887a
Dheeraj <dheerajb@buffalo.edu>
2020-06-12 12:44:43 -0400

    trying opensource script from https://gist.github.com/gdamjan/55a8b9eec6cf7b771f92021d93b87b2c

--- certcheck.py
+++ certcheck.py
@@ -9,7 +9,13 @@ import OpenSSL
 from subprocess import Popen, PIPE
 from datetime import datetime
 from xml.dom import minidom
+from OpenSSL import SSL
+from cryptography import x509
+from cryptography.x509.oid import NameOID
+import idna
 
+from socket import socket
+from collections import namedtuple
 
 def get_certificate(host, port=443, timeout=4):
     context = ssl.create_default_context()
@@ -46,7 +52,7 @@ hosts = root.findall('host')    # find all elements named ‘host’
 for hostname in root.iter('hostname'):
     print(hostname.attrib['name'])
     try:
-        analyze_cert(str(hostname.attrib['name']))
+        get_certificate(hostname.attrib['name'],443)
     except Exception as ex:
         print("Exception raised"+str(ex))
     finally:
@@ -55,3 +61,25 @@ for hostname in root.iter('hostname'):
 remove_file = 'rm test.xml'.split()
 run_command(remove_file)
 
+HostInfo = namedtuple(field_names='cert hostname peername', typename='HostInfo')
+
+def get_certificate(hostname, port):
+    hostname_idna = idna.encode(hostname)
+    sock = socket()
+
+    sock.connect((hostname, port))
+    peername = sock.getpeername()
+    ctx = SSL.Context(SSL.SSLv23_METHOD) # most compatible
+    ctx.check_hostname = False
+    ctx.verify_mode = SSL.VERIFY_NONE
+
+    sock_ssl = SSL.Connection(ctx, sock)
+    sock_ssl.set_connect_state()
+    sock_ssl.set_tlsext_host_name(hostname_idna)
+    sock_ssl.do_handshake()
+    cert = sock_ssl.get_peer_certificate()
+    crypto_cert = cert.to_cryptography()
+    sock_ssl.close()
+    sock.close()
+
+    return HostInfo(cert=crypto_cert, peername=peername, hostname=hostname)
